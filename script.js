5857f0137ccfb8e056a30d707727f2caa236278b
aquaritic <krish.s.lad@gmail.com>
2026-05-03 11:29:11 -0500

    Made the actual keyboard aspect into the code, where lowercase letters are smaller droplets and uppercase letters are bigger. The color is also dependent on the size now too.

--- script.js
+++ script.js
@@ -6,21 +6,13 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const surface = canvas.height * 0.75;
 
-function droplet(){
-    droplets.push({
-        x: Math.random() * canvas.width,
-        y: 0,
-        size: Math.random() * 25 + 5,
-        speed: Math.random() * 6 + 1
-    });
-}
-
-function ripple(x, y){
+function ripple(x, y, color){
     ripples.push({
         x,
         y,
         radius: Math.random() * 7 + 3,
-        opacity: Math.random() * .5 + 1
+        opacity: Math.random() * .5 + 1,
+        color
     });
 }
 
@@ -32,42 +24,56 @@ function animation() {
         d.y += d.speed;
         ctx.beginPath();
         ctx.arc(d.x, d.y, d.size, 0, Math.PI *2);
-        if (d.speed > 3) {
-            ctx.fillStyle = "lightblue";
-        } else {
-            ctx.fillStyle = "blue";
-        }
+        ctx.fillStyle = d.color;
         ctx.fill();
 
         if (d.y >= surface) {
-            ripple(d.x, surface);
+            ripple(d.x, surface, d.color);
             droplets.splice(i, 1);
             i--;
         }
-
-        for (let i = 0; i < ripples.length; i++){
+    }
+    for (let i = 0; i < ripples.length; i++){
             let r = ripples[i];
             r.radius += 2;
             r.opacity -= .01;
 
             ctx.beginPath();
             ctx.arc(r.x, r.y, r.radius, 0, Math.PI*2);
-            ctx.strokeStyle = `rgba(173,216,230,${r.opacity})`;
+            ctx.strokeStyle = r.color;
+            ctx.globalAlpha = r.opacity;
             ctx.stroke();
-
+            ctx.globalAlpha = 1;
             if (r.opacity <= 0) {
                 ripples.splice(i, 1);
                 i--;
             }
         }
-    }
     requestAnimationFrame(animation);
 }
 
+document.addEventListener("keydown", (event) => {
+    let size;
+    let speed;
+    let color;
+
+    if (event.key >= "a" && event.key <= "z"){
+        size = 10;
+        speed = 3;
+        color = "cyan";
+    } else if (event.key >= "A" && event.key <= "Z"){
+        size = 35;
+        speed = 8;
+        color = "darkblue";
+    }
+
+    droplets.push({
+        x: Math.random() * canvas.width,
+        y: 0,
+        size: size,
+        speed: speed,
+        color: color
+    });
+});
+
 animation();
-
-setInterval(() => {
-    droplet();
-}, 250);
-
-
