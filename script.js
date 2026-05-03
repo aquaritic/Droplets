2ceb06b3122574d55d9202d42afcce12b977dbb5
aquaritic <krish.s.lad@gmail.com>
2026-05-03 18:55:17 -0500

    Made more event listeners for each number from 1-9, making each ones size different as well as speed, I also added a click event listener to spawn rain where you click and added a snow option

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ const ripples = [];
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const surface = canvas.height * 0.75;
+const mode = document.getElementById("mode");
+let isSnow = false;
+
+mode.addEventListener("click", () =>{
+    isSnow = !isSnow
+});
 
 function ripple(x, y, color){
     ripples.push({
@@ -62,30 +68,141 @@ document.addEventListener("keydown", (event) => {
         x = Math.random() * canvas.width/2;
         size = 10;
         speed = 3;
-        color = "cyan";
+        if (isSnow){
+            color = "white"
+        } else {
+            color = "cyan"
+        }
     } else if (event.key >= "n" && event.key <= "z"){
         x = Math.random() * (canvas.width - canvas.width/2)  + canvas.width/2;
         size = 10;
         speed = 3;
-        color = "cyan";
+        if (isSnow){
+            color = "white"
+        } else {
+            color = "cyan"
+        }
     } else if (event.key >= "A" && event.key <= "M"){
         x = Math.random() * canvas.width/2;
         size = 40;
         speed = 20;
-        color = "darkblue";
+        if (isSnow){
+            color = "white"
+        } else {
+            color = "darkblue"
+        }
     } else if (event.key >= "N" && event.key <= "Z"){
         x = Math.random() * (canvas.width - canvas.width/2) + canvas.width/2;
         size = 40;
         speed = 20;
-        color = "darkblue";
+        if (isSnow){
+            color = "white"
+        } else {
+            color = "darkblue"
+        }
+    } else if (event.key == "1"){
+        x = Math.random() * canvas.width;
+        size = 2;
+        speed = 2;
+        if (isSnow){
+            color = "white"
+        } else {
+            color = "blue"
+        }
+    } else if (event.key == "2"){
+        x = Math.random() * canvas.width;
+        size = 4;
+        speed = 4;
+        if (isSnow){
+            color = "white"
+        } else {
+            color = "blue"
+        }
+    } else if (event.key == "3"){
+        x = Math.random() * canvas.width;
+        size = 6;
+        speed = 6;
+        if (isSnow){
+            color = "white"
+        } else {
+            color = "blue"
+        }
+    } else if (event.key == "4"){
+        x = Math.random() * canvas.width;
+        size = 8;
+        speed = 8;
+        if (isSnow){
+            color = "white"
+        } else {
+            color = "blue"
+        }
+    } else if (event.key == "5"){
+        x = Math.random() * canvas.width;
+        size = 10;
+        speed = 10;
+        if (isSnow){
+            color = "white"
+        } else {
+            color = "blue"
+        }
+    } else if (event.key == "6"){
+        x = Math.random() * canvas.width;
+        size = 12;
+        speed = 12;
+        if (isSnow){
+            color = "white"
+        } else {
+            color = "blue"
+        }
+    } else if (event.key == "7"){
+        x = Math.random() * canvas.width;
+        size = 14;
+        speed = 14;
+        if (isSnow){
+            color = "white"
+        } else {
+            color = "blue"
+        }
+    } else if (event.key == "8"){
+        x = Math.random() * canvas.width;
+        size = 16;
+        speed = 16;
+        if (isSnow){
+            color = "white"
+        } else {
+            color = "blue"
+        }
+    } else if (event.key == "9"){
+        x = Math.random() * canvas.width;
+        size = 18;
+        speed = 18;
+        if (isSnow){
+            color = "white"
+        } else {
+            color = "blue"
+        }
     }
-
     droplets.push({
         x: x,
         y: 0,
         size: size,
         speed: speed,
         color: color
+    });
+});
+
+canvas.addEventListener("click", (event) => {
+    if (isSnow){
+            color = "white";
+        } else {
+            color = "turqoise";
+        }
+    droplets.push({
+        x: event.clientX,
+        y: event.clientY,
+        size: 25,
+        speed: 15,
+        color
     });
 });
 
